402ddec96e6d4184f22cf7751d04f54a6b9c55e7
Prince-ABES <prk988110@gmail.com>
2026-09-09 23:01:06 +0530

    prepare backend for deployment

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 connectDB()
   .then(() => {
-    app.listen(port, () => {
+    app.listen(port, "0.0.0.0", () => {
       console.log(`Server is running on port ${port}`);
     });
   })
